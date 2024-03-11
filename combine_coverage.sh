1c7d8f5396070bc8d1980a95605acd3881ac0543
DMITRIY PEDCHENKO <dypedchenk@mts.ru>
2024-03-11 13:10:11 +0300

    [DOP-13277] Collect coverage results

--- combine_coverage.sh
+++ combine_coverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 root_path=$(dirname $(realpath $0))
-coverage combine --rcfile=syncmaster/tests/.coveragerc
-coverage xml --rcfile=syncmaster/tests/.coveragerc -o $root_path/reports/coverage.xml -i
+coverage combine
+coverage xml -o $root_path/reports/coverage.xml -i
