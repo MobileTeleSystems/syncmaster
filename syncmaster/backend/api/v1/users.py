09b073f5f95dc400ff1df83568ebb6b23d8a2aaf
Ilyas Gasanov <izgasanov@mts.ru>
2024-09-27 09:57:50 +0300

    [DOP-19779] Delete user modification endpoints

--- syncmaster/backend/api/v1/users.py
+++ syncmaster/backend/api/v1/users.py
@@ -7,9 +7,7 @@ from fastapi import APIRouter, Depends, Query
 from syncmaster.backend.api.deps import UnitOfWorkMarker
 from syncmaster.backend.services import UnitOfWork, get_user
 from syncmaster.db.models import User
-from syncmaster.exceptions import ActionNotAllowedError
-from syncmaster.schemas.v1.status import StatusResponseSchema
-from syncmaster.schemas.v1.users import ReadUserSchema, UpdateUserSchema, UserPageSchema
+from syncmaster.schemas.v1.users import ReadUserSchema, UserPageSchema
 
 logger = logging.getLogger(__name__)
 
@@ -46,39 +44,3 @@ async def read_user(
 ) -> ReadUserSchema:
     user = await unit_of_work.user.read_by_id(user_id=user_id, is_active=True)
     return ReadUserSchema.from_orm(user)
-
-
-@router.patch("/users/{user_id}")
-async def update_user(
-    user_id: int,
-    user_data: UpdateUserSchema,
-    current_user: User = Depends(get_user(is_active=True)),
-    unit_of_work: UnitOfWork = Depends(UnitOfWorkMarker),
-) -> ReadUserSchema:
-    if user_id != current_user.id and not current_user.is_superuser:
-        raise ActionNotAllowedError
-    async with unit_of_work:
-        change_user = await unit_of_work.user.update(user_id=user_id, data=user_data.dict())
-    return ReadUserSchema.from_orm(change_user)
-
-
-@router.post("/users/{user_id}/activate", dependencies=[Depends(get_user(is_superuser=True))])
-async def activate_user(
-    user_id: int,
-    unit_of_work: UnitOfWork = Depends(UnitOfWorkMarker),
-) -> StatusResponseSchema:
-    async with unit_of_work:
-        user = await unit_of_work.user.update(user_id=user_id, data={"is_active": True})
-    logger.info("User %s active=%s id=%d", user, user.is_active, user.id)
-    return StatusResponseSchema(ok=True, status_code=200, message="User was activated")
-
-
-@router.post("/users/{user_id}/deactivate", dependencies=[Depends(get_user(is_superuser=True))])
-async def deactivate_user(
-    user_id: int,
-    unit_of_work: UnitOfWork = Depends(UnitOfWorkMarker),
-):
-    async with unit_of_work:
-        user = await unit_of_work.user.update(user_id=user_id, data={"is_active": False})
-    logger.info("User %s active=%s id=%d", user, user.is_active, user.id)
-    return StatusResponseSchema(ok=True, status_code=200, message="User was deactivated")
