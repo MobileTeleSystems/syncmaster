eaf9130707b40430ccaba8565c6a300309bd7108
Ilyas Gasanov <izgasanov@mts.ru>
2024-11-05 16:20:43 +0300

    [DOP-20962] Add scheduler integration test

--- tests/test_integration/test_scheduler/fixtures.py
+++ tests/test_integration/test_scheduler/fixtures.py
@@ -8,7 +8,10 @@ from syncmaster.scheduler.transfer_job_manager import TransferJobManager
 from syncmaster.worker.config import celery
 
 
-@pytest.fixture
+@pytest.fixture(
+    scope="session",
+    params=[pytest.param("scheduler", marks=[pytest.mark.scheduler])],
+)
 def mock_send_task_to_tick(mocker: MockerFixture):
     original_to_thread = asyncio.to_thread
     return mocker.patch(
@@ -17,7 +20,10 @@ def mock_send_task_to_tick(mocker: MockerFixture):
     )
 
 
-@pytest.fixture
+@pytest.fixture(
+    scope="session",
+    params=[pytest.param("scheduler", marks=[pytest.mark.scheduler])],
+)
 def mock_add_job(mocker: MockerFixture, transfer_job_manager: TransferJobManager):
     original_add_job = transfer_job_manager.scheduler.add_job
     return mocker.patch.object(
