8b0151271bfcff85f75f99796cd9cd70df366449
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-02-05 16:21:07 +0300

    [DOP-23743] All hardcoded passwords in this repo are used ONLY for tests

--- docker/samba/custom_entrypoint.sh
+++ docker/samba/custom_entrypoint.sh
@@ -3,4 +3,4 @@
 # allow create files and directories
 mkdir -p /share/folder
 chmod 0777 /share/folder
-/entrypoint.sh -u "1000:1000:syncmaster:syncmaster:test_only" -s "SmbShare:/share/folder:rw:syncmaster"
+/entrypoint.sh -u "1000:1000:syncmaster:syncmaster:123UsedForTestOnly@!" -s "SmbShare:/share/folder:rw:syncmaster"
