4662e5bbf01b84bccea123b24c7e17b0e455d2dc
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-04-11 17:43:14 +0300

    [DOP-25451] Update run status in separated commit

--- syncmaster/worker/transfer.py
+++ syncmaster/worker/transfer.py
@@ -12,7 +12,6 @@ from sqlalchemy.orm import Session, selectinload
 
 from syncmaster.db.models import AuthData, Run, Status, Transfer
 from syncmaster.db.repositories.utils import decrypt_auth_data
-from syncmaster.exceptions.run import RunNotFoundError
 from syncmaster.settings.log import setup_logging
 from syncmaster.worker.base import WorkerTask
 from syncmaster.worker.celery import app as celery
@@ -33,66 +32,69 @@ def run_transfer_task(self: WorkerTask, run_id: int) -> None:
 
 
 def run_transfer(run_id: int, engine: Engine, settings: WorkerAppSettings):
-    with Session(engine) as session:
-        run = session.scalar(
-            select(Run)
-            .where(
-                Run.id == run_id,
-                Run.status == Status.CREATED,
-            )
-            .options(
-                selectinload(Run.transfer),
-                selectinload(Run.transfer).selectinload(Transfer.group),
-                selectinload(Run.transfer).selectinload(Transfer.source_connection),
-                selectinload(Run.transfer).selectinload(Transfer.target_connection),
-            ),
-        )
-        if run is None:
-            raise RunNotFoundError
-
-        q_source_auth_data = select(AuthData).where(AuthData.connection_id == run.transfer.source_connection.id)
-        q_target_auth_data = select(AuthData).where(AuthData.connection_id == run.transfer.target_connection.id)
-
-        target_auth_data = decrypt_auth_data(session.scalar(q_target_auth_data).value, settings)
-        source_auth_data = decrypt_auth_data(session.scalar(q_source_auth_data).value, settings)
-
-        logger.info("Starting run")
-        run.status = Status.STARTED
-        run.started_at = datetime.now(tz=timezone.utc)
-        run.log_url = Template(settings.worker.log_url_template).render(run=run, correlation_id=correlation_id.get())
-        session.add(run)
-        session.commit()
-        session.expunge_all()
-
     try:
+        with Session(engine) as session:
+            run = session.scalar(
+                select(Run)
+                .where(
+                    Run.id == run_id,
+                    Run.status == Status.CREATED,
+                )
+                .options(
+                    selectinload(Run.transfer),
+                    selectinload(Run.transfer).selectinload(Transfer.group),
+                    selectinload(Run.transfer).selectinload(Transfer.source_connection),
+                    selectinload(Run.transfer).selectinload(Transfer.target_connection),
+                ),
+            )
+            if run is None:
+                logger.info("Run not found or already executed by someone else")
+                return
+
+            source_connection = run.transfer.source_connection
+            q_source_auth_data = select(AuthData).where(AuthData.connection_id == run.transfer.source_connection.id)
+            source_auth_data = decrypt_auth_data(session.scalar(q_source_auth_data).value, settings)
+
+            q_target_auth_data = select(AuthData).where(AuthData.connection_id == run.transfer.target_connection.id)
+            target_connection = run.transfer.target_connection
+            target_auth_data = decrypt_auth_data(session.scalar(q_target_auth_data).value, settings)
+
+            logger.info("Starting run")
+            run.status = Status.STARTED
+            run.started_at = datetime.now(tz=timezone.utc)
+            run.log_url = Template(settings.worker.log_url_template).render(
+                run=run,
+                correlation_id=correlation_id.get(),
+            )
+            session.add(run)
+            session.commit()
+
         controller = TransferController(
             settings=settings,
             run=run,
-            source_connection=run.transfer.source_connection,
-            target_connection=run.transfer.target_connection,
+            source_connection=source_connection,
+            target_connection=target_connection,
             source_auth_data=source_auth_data,
             target_auth_data=target_auth_data,
         )
         controller.perform_transfer()
     except Exception as e:
         status = Status.FAILED
-        logger.exception("Run %r failed", run.id)
+        logger.exception("Run %r failed", run_id)
         exception = e
     else:
         status = Status.FINISHED
-        logger.info("Run %r was successful", run.id)
+        logger.info("Run %r was successful", run_id)
         exception = None
 
     with Session(engine) as session:
         run = session.get(Run, run_id)
-        if run is None:
-            raise RunNotFoundError
-
-        logger.info("Updating run status")
-        run.status = status
-        run.ended_at = datetime.now(tz=timezone.utc)
-        session.add(run)
-        session.commit()
+        if run:
+            logger.info("Updating run status")
+            run.status = status
+            run.ended_at = datetime.now(tz=timezone.utc)
+            session.add(run)
+            session.commit()
 
     if exception is not None:
         raise exception
