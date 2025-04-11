f7a928f9569f37bf004ece7adff710308afb11dc
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-04-11 16:52:34 +0300

    [DOP-25451] Bump version

--- syncmaster/__init__.py
+++ syncmaster/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2023-2024 MTS PJSC
 # SPDX-License-Identifier: Apache-2.0
 
-_raw_version = "0.2.2"
+_raw_version = "0.2.3"
 # version always contain only release number like 0.0.1
 __version__ = ".".join(_raw_version.split(".")[:3])  # noqa: WPS410
