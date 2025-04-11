4f9f273c1ab25562487d0322410594e57c74c408
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-04-11 20:47:24 +0300

    [DOP-25451] Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ author = "DataOps.ETL"
 # The short X.Y version.
 
 # this value is updated automatically by `poetry version ...` and poetry-bumpversion plugin
-ver = Version.parse("0.2.3")
+ver = Version.parse("0.2.4")
 version = ver.base_version
 # The full version, including alpha/beta/rc tags.
 release = ver.public
