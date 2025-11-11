9d2124b7f1122fbda1e2931191af4458a8292163
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-11-11 17:17:31 +0300

    Fix S3 and IcebergRestS3 tests

--- tests/test_unit/test_connections/test_db_connection/test_update_iceberg_connection.py
+++ tests/test_unit/test_connections/test_db_connection/test_update_iceberg_connection.py
@@ -2,6 +2,7 @@ import pytest
 from httpx import AsyncClient
 
 from tests.mocks import MockConnection, UserTestRoles
+from tests.test_unit.utils import fetch_connection_json
 
 pytestmark = [pytest.mark.asyncio, pytest.mark.server, pytest.mark.iceberg]
 
@@ -38,11 +39,13 @@ async def test_developer_plus_can_update_iceberg_rest_s3_connection(
     role_developer_plus: UserTestRoles,
 ):
     user = group_connection.owner_group.get_member_of_role(role_developer_plus)
+    connection_json = await fetch_connection_json(client, user.token, group_connection)
 
     result = await client.put(
         f"v1/connections/{group_connection.id}",
         headers={"Authorization": f"Bearer {user.token}"},
         json={
+            **connection_json,
             "type": group_connection.type,
             "connection_data": {
                 "metastore_url": "http://rest.domain.com:8000",
