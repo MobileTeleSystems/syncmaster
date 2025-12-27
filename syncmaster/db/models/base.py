2dc21f83fc0d96157e3c5a3b46c81bc1e624a91e
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-12-27 14:31:38 +0300

    Fix migrations

--- syncmaster/db/models/base.py
+++ syncmaster/db/models/base.py
@@ -4,15 +4,20 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
 
 convention = {
+    "all_column_names": lambda constraint, table: "_".join(
+        [column.name for column in constraint.columns.values()],
+    ),
     "ix": "ix__%(table_name)s__%(all_column_names)s",
     "uq": "uq__%(table_name)s__%(all_column_names)s",
     "ck": "ck__%(table_name)s__%(constraint_name)s",
-    "fk": ("fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s"),
+    "fk": "fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s",
     "pk": "pk__%(table_name)s",
 }
+
+syncmaster_metadata = MetaData(naming_convention=convention)  # type: ignore[arg-type]
 
 
 # as_declarative decorator causes mypy errors.
 # Use inheritance from DeclarativeBase.
 class Base(DeclarativeBase):
-    metadata = MetaData(naming_convention=convention)
+    metadata = syncmaster_metadata
