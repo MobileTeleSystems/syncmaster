0e72797090ee0f6c416e67cd5535471510d94341
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2026-08-06 12:42:24 +0300

    [DOP-39512] Remove fastapi dependeciy_override and Stub

--- syncmaster/server/services/auth.py
+++ syncmaster/server/services/auth.py
@@ -7,7 +7,3 @@ from syncmaster.server.providers.auth.base_provider import AuthProvider
 
 async def get_auth_provider(request: Request) -> AuthProvider:
     return request.app.state.auth_provider
-
-
-async def get_personal_token_provider(request: Request) -> AuthProvider:
-    return request.app.state.personal_token_auth_provider
